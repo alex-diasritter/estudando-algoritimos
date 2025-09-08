2327ee3b3d5424a3b753c3367faf13254e6eba95
Luz-Helena <>
2025-09-08 13:16:48 -0300

    tornando o algoritimo mais claro

--- src/main/java/org/leetcode/twosum/Tentativa1.java
+++ src/main/java/org/leetcode/twosum/Tentativa1.java
@@ -7,27 +7,32 @@ import java.util.Map;
 
 public class Tentativa1 {
     public static void main(String[] args) {
+
         List<Integer> list = new ArrayList<>(List.of(2, 3, 3, 6, 4, 7, 8, 5, 7));
+
         var r = twoSum(list, 13);
+
+        System.out.println("Main: " + r);
 
     }
 
     public static List<Integer> twoSum(List<Integer> list, int target) {
-        /*
-        definir hash
-        preciso iterar o array
-        checar se o numero do index i + index anterior (i-1) é igual ao target
-        se não for, adiciona o numero do index i como chave em um hashmap, e o index é adicionado como valor no hashmap
-         */
+
         Map<Integer, Integer> valores = new HashMap<>();
-        for (int i = 0; i < list.size(); i++) {
-            int complemento = target - list.get(i);
+
+        for (int index = 0; index < list.size(); index++) {
+
+            int complemento = target - list.get(index);
+
             if (valores.containsKey(complemento)) {
-                System.out.println("Sucesso.");
-                System.out.println("Retornando os índices: [" + valores.get(complemento) + ", " + i + "]");
-                return new ArrayList<>(List.of(valores.get(complemento), i));
+
+                System.out.println("Retornando os índices: [" + valores.get(complemento)+ ", " + index + "]");
+                System.out.println("Valores: " + complemento + " (do indice: " + valores.get(complemento) + ") + " + list.get(index) + " (do indice: " + index + ") = " + target);
+
+                return new ArrayList<>(List.of(valores.get(complemento), index));
             }
-            valores.put(list.get(i), i);
+
+            valores.put(list.get(index), index);
         }
         throw new IllegalArgumentException();
     }
